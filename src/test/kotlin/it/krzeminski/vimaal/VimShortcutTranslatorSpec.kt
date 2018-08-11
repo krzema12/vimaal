9655fd38198d50a8e822b41d187a2a9638db7364
Piotr Krzeminski <git@krzeminski.it>
2018-08-11 23:51:31 +0200

    Refactor shortcut translator tests

--- src/test/kotlin/it/krzeminski/vimaal/VimShortcutTranslatorSpec.kt
+++ src/test/kotlin/it/krzeminski/vimaal/VimShortcutTranslatorSpec.kt
@@ -9,6 +9,9 @@ class VimShortcutTranslatorSpec : SpecnazKotlinJUnit(VimShortcutTranslator::clas
     lateinit var textChangeListenerMock: TextChangeListener
     lateinit var vimShortcutTranslator: VimShortcutTranslator
 
+    fun keysPressed(keys: String) =
+            keys.forEach { character -> vimShortcutTranslator.keyPressed(character) }
+
     it.beginsEach {
         textChangeListenerMock = spyk()
         vimShortcutTranslator = VimShortcutTranslator(textChangeListenerMock)
@@ -16,19 +19,19 @@ class VimShortcutTranslatorSpec : SpecnazKotlinJUnit(VimShortcutTranslator::clas
 
     it.describes("deleting") {
         it.should("do nothing when 'd' is pressed") {
-            vimShortcutTranslator.keyPressed('d')
+            keysPressed("d")
 
             verify { textChangeListenerMock wasNot Called }
         }
 
         it.should("remove one line when 'dd' is pressed") {
-            repeat(2) { vimShortcutTranslator.keyPressed('d') }
+            keysPressed("dd")
 
             verify(exactly = 1) { textChangeListenerMock.onLinesRemoved(quantity = 1) }
         }
 
         it.should("remove two lines when 'dd' is pressed twice") {
-            repeat(4) { vimShortcutTranslator.keyPressed('d') }
+            keysPressed("dddd")
 
             verify(exactly = 2) { textChangeListenerMock.onLinesRemoved(quantity = 1) }
         }
